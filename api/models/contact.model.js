991e5e39db725113ae712afedc73a759a28de996
Marianne Antenor <21480725@latrobe.students.edu.au>
2023-10-22 10:41:09 +1100

    added the contact name column in db

--- api/models/contact.model.js
+++ api/models/contact.model.js
@@ -6,6 +6,13 @@ module.exports = (sequelize, Sequelize) => {
             primaryKey: true,
         },
         // DEFINE YOUR MODEL HERE
+        name: {   // This is the new "contact name" column
+            type: Sequelize.STRING,
+            allowNull: false,   // This makes the column NOT NULL
+            validate: {
+                notEmpty: true,  // This makes sure the string is not empty
+            }
+        }
     });
   
     return Contact;
